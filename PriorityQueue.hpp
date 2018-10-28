aa3842297ac2150ec9f688fcccfc6a186714096e
TaddyMason <43047335+TaddyMason@users.noreply.github.com>
2018-10-28 17:29:14 -0400

    Update PriorityQueue.hpp

--- PriorityQueue.hpp
+++ PriorityQueue.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "Event.cpp"
 #include <string>
 using namespace std;
 template <class T> class PriorityQueue{
@@ -7,10 +8,30 @@ template <class T> class PriorityQueue{
     int arraySize;
     int simTime;
   public:
+    MyPriorityQueue();
     void insert(T e);
     void remove();
-    int size;
+    int size();
+    PriorityQueue& operator=(const PriorityQueue<T>& other){//incoming tote has the same size and values as the main
+      arraySize = other.arraySize;
+      arrq = new T[arraySize];
+      for(int i = 0; i < arraySize; i++){
+        arrq[i] = other.arrq[i];
+      }
+    }
+    friend ostream& operator<<(ostream& os, const PriorityQueue<T>& list){// prints out the entire tote to the screen
+      int otherTemp = list.arrq[list.size-1];
+      os << "[";
+      for(int i = 0; i < list.arraySize - 1; i++){
+        int temp = list.arrq[i];
+        os << temp << " -- ";
+      }
+      os << otherTemp;
+      os << "]";
+      return os;
+    }
   };
+  template class PriorityQueue<Event>;
   template class PriorityQueue<int>;
   template class PriorityQueue<float>;
   template class PriorityQueue<double>;
