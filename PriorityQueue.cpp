79318b49a13adda5b4583c0d4f7de651d14f9879
TaddyMason <43047335+TaddyMason@users.noreply.github.com>
2018-10-30 16:00:54 -0400

    Update PriorityQueue.cpp

--- PriorityQueue.cpp
+++ PriorityQueue.cpp
@@ -1,13 +1,13 @@
-#include "customer.cpp"
 #include "PriorityQueue.hpp"
 #include <iostream>
 using namespace std;
 
 
-
-template <class T> void PriorityQueue<T>::insert(T e){
-  T * arrq = new T[100000];
-  int arraySize = 0;
+template <class T> PriorityQueue<T>::PriorityQueue() {
+  arraySize = 0;
+  arrq = new T[10000];
+}
+template <class T> void  PriorityQueue<T>::insert(T e){
   if(arraySize == 0){
     arrq[1] = e;
     arraySize = 1;
@@ -15,8 +15,8 @@ template <class T> void PriorityQueue<T>::insert(T e){
     arrq[arraySize + 1] = e;
     arraySize++;
     int parent = arraySize;
-    T temp();
-    T testParent();
+    T temp;
+    T testParent;
     while(parent != 1){
       if((parent % 2) != 0){
         parent = parent - 1;
@@ -24,34 +24,34 @@ template <class T> void PriorityQueue<T>::insert(T e){
       parent = parent / 2;
       temp = arrq[arraySize];
       testParent = arrq[parent];
-      double testParentTime = testParent.getSimTime();
-      double tempTime = temp.getSimTime();
-      if(testParentTime > tempTime){
+      if(testParent > temp){
         arrq[arraySize] = arrq[parent];
         arrq[parent] = temp;
       }
     }
   }
 }
-  template <class T> void PriorityQueue<T>::remove(){
-    T temp = arrq[1];
-    T temp2;
-    arrq[1] = arrq[arraySize];
-    arrq[arraySize] = arrq[arraySize + 1];
-    arraySize = arraySize - 1;
-    int i = 1;
-    while(i < arraySize){
-      if(arrq[(2*i)].getSimTime() > arrq[(2*i+ 1)].getSimTime()){
-        arrq[i] = arrq[2*i + 1];
-        arrq[2*i + 1] = temp;
-        i = 2*i + 1;
-      }else{
-        arrq[i] = arrq[2*i];
-        arrq[2*i] = temp;
-        i = 2*i;
-      }
+template <class T> T PriorityQueue<T>::remove(){
+  T temp = arrq[1];
+  T temp2 = arrq[1];
+  arrq[1] = arrq[arraySize];
+  arrq[arraySize] = arrq[arraySize + 1];
+  arraySize = arraySize - 1;
+  int i = 1;
+  while(i < arraySize){
+    if(arrq[(2*i)] > arrq[(2*i+ 1)]){
+      arrq[i] = arrq[2*i + 1];
+      arrq[2*i + 1] = temp;
+      i = 2*i + 1;
+    }else{
+      temp = arrq[2 * i];
+      arrq[i] = arrq[2*i];
+      arrq[2*i] = temp;
+      i = 2*i;
     }
   }
-  template <class T> int PriorityQueue<T>::size(){
-    return arraySize;
-  }
+  return temp2;
+}
+template <class T> int PriorityQueue<T>::size(){
+  return arraySize;
+}
